f67257eb12f1980cf18b786460c9eed62b5c49e2
Simon Jäger <simonjager@hotmail.com>
2025-12-10 21:47:57 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Imports
+import argparse
+import os
+import sys
+
+parser = argparse.ArgumentParser()
+
+# Take arguments for album location
+
+# Take arguments for string to replace
+
+# Take arguments for new name
+
+# Find files
+
+# Apply changes to file
+
