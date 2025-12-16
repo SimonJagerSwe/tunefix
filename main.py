923c21846262d7168ee4434baef8355393ee4630
Simon Jäger <simonjager@hotmail.com>
2025-12-16 22:04:46 +0100

    argparsing works

--- main.py
+++ main.py
@@ -6,21 +6,28 @@ import sys
 
 # Main function
 def main():
-    a_dir = r"C:\Users\simon\OneDrive\Dokument\Soulseek Downloads\complete\ahyonfelihufs\Awake"
-    # get_file_dir(a_dir)
+    # a_dir = r"C:\Users\simon\OneDrive\Dokument\Soulseek Downloads\complete\ahyonfelihufs\Awake"
+    a_dir = rf""
+    parse_args(a_dir)
+    print(a_dir)
     identify_files(a_dir)
 
-# Get file directory
-def get_file_dir(a_dir):
+# Parse arguments
+def parse_args(a_dir):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", help="The directory containing the tracks you want to change the names of")
+    parser.add_argument("-r", "--replace", help="Part of title to be replaced")
+    # parser.add_argument("-n", "--new", help="New replacement")
+    args = parser.parse_args()
+    a_dir = rf"{args.directory}"
+    print(rf"{args.directory}")
+    # print(a_dir)    
+    print(args.replace)
+    # print(args.new)
+
     return a_dir
 
-# Get file name part to replace
-def to_be_replaced():
-    ...
 
-# Get file name replacement
-def to_replace():
-    ...
 
 # Walk through directory to identify files
 def identify_files(a_dir):
