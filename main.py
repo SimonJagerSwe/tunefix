b76a80a13bd37708c5af31c94cd3c25787857ccd
Simon Jäger <simonjager@hotmail.com>
2025-12-16 17:12:12 +0100

    File identification works

--- main.py
+++ main.py
@@ -3,40 +3,31 @@ import argparse
 import os
 import sys
 
-A_DIR = ""
 
 # Main function
 def main():
-    get_file_dir(A_DIR)
-    identify_files()
+    a_dir = r"C:\Users\simon\OneDrive\Dokument\Soulseek Downloads\complete\ahyonfelihufs\Awake"
+    # get_file_dir(a_dir)
+    identify_files(a_dir)
 
 # Get file directory
-def get_file_dir(A_DIR):
-    # Hard-coded file directory, for test
-    A_DIR = r"C:\Users\simon\OneDrive\Dokument\Soulseek Downloads\complete\ahyonfelihufs\Awake"
-    return A_DIR
+def get_file_dir(a_dir):
+    return a_dir
 
 # Get file name part to replace
 def to_be_replaced():
     ...
-
 
 # Get file name replacement
 def to_replace():
     ...
 
 # Walk through directory to identify files
-def identify_files():
-    # print(a_dir)
-    for root, dirs, files in os.walk(A_DIR):
-        # if os.path.basename(root) != "modules":
-        #     continue
-        # print(f"Root: {root}")
-        # print(f"Root type: {type(root)}")
-        # print(f"Dirs: {dirs}")
-        # print(f"Dirs type: {type(dirs)}")
-        print(f"File: {files}")
-        # print(f"Files type: {type(files)}")
+def identify_files(a_dir):
+    for root, dirs, files in os.walk(a_dir):
+        # print(f"File: {files}")
+        for file in files:
+            print(file)
 
 
 
