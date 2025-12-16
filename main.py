bce02aea8e93d0fa76f07ec24dcc57f3adc92ac2
Simon Jäger <simonjager@hotmail.com>
2025-12-16 22:42:35 +0100

    cleanup

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def main():
 
 # Parse arguments
 def parse_args(dir_arg):
-    print(dir_arg)
+    # print(dir_arg)
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory", help="The directory containing the tracks you want to change the names of")
     parser.add_argument("-r", "--replace", help="Part of title to be replaced")
     # parser.add_argument("-n", "--new", help="New replacement")
     args = parser.parse_args()
     dir_arg = rf"{args.directory}"
-    print(dir_arg)    
+    # print(dir_arg)    
     # print(args.replace)
     # print(args.new)
     return dir_arg
